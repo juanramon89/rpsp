4acc7eb03c6c69dafc0370118fa51f4a0458d82a
juanramon89 <juanramalva@gmail.com>
2014-05-19 21:36:32 +0200

    Arreglado recogida de datos del servidor y añadido fichero que recoge el resultado

--- PeticionCliente/src/serpis/psp/Cliente.java
+++ PeticionCliente/src/serpis/psp/Cliente.java
@@ -1,14 +1,10 @@
 package serpis.psp;
 
-import java.io.DataInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.Scanner;
 
 public class Cliente {
@@ -21,29 +17,33 @@ public class Cliente {
 		System.out.println("Cliente envia peticion:");
 		Socket socket = new Socket(SERVER_IP,SERVER_PORT);
 		
-		String peticion = "GET /index.html HTTP/1.0";
-		PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-		//Scanner scanner = new Scanner(socket.getInputStream());
+		String peticion = "GET /index.html HTTP/1.0 \r\n"+
+							"\r\n";
 		
+		String file = "/home/ramon/rpsp/PeticionCliente/resultado.txt";
+		PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+		Scanner scanner = new Scanner(socket.getInputStream());
+		FileWriter fichero = new FileWriter(file);
 		
 		printWriter.print(peticion);
 		printWriter.flush();
 		Thread.sleep(1000);
 		System.out.println("Peticion enviada: "+ peticion);
 		
-		 DataInputStream entrada;
-		 try {
-		 entrada = new DataInputStream( socket.getInputStream());
-		 
-		 while (entrada.read() != -1){
-			 String linea = entrada.readLine();
-			 System.out.println(linea);
+		 try{
+			 fichero = new FileWriter(file);
+			 
+			 while (true){
+				 String linea = scanner.nextLine();
+				 System.out.println(linea);
+				 fichero.write(linea);
+			 }
+				 
+		 }catch (Exception e){
+			 e.printStackTrace();
 		 }
-		 
-		 } catch( IOException e ) {
-		 System.out.println( e );
-		 }
-		
+		 fichero.close();
+		socket.close();
 	}
 
 }
